4cea166bbb7afa34ced9f6d8e68019f9e72218da
Sachin Tripathi <sachintripathis84@gmail.com>
2019-06-23 16:53:24 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,22 @@
+import tweepy
+
+consumer_key = ''
+consumer_secret =''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
+query = 'mr robot'
+for tweet in tweepy.Cursor(api.search,q=query,count=100,result_type="recent",include_entities=True,lang='eng').items():
+    print(tweet.text)
+
+
+
 
